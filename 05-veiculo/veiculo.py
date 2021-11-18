306e4ff5749ad6680232283cf4ee8f04ef0fa948
Ruan Ramírez <ruanramirez01@gmail.com>
2021-11-18 20:10:52 -0300

    Atividade 17 de novembro de 2021

--- 05-veiculo/veiculo.py
+++ 05-veiculo/veiculo.py
@@ -42,24 +42,31 @@ segundo_veiculo = Veiculo('TTB3141', 'Honda', 'GX-1', 2017, 12145)
 
 lista_veiculos = []
 
-def cadastrarVeiculo(veiculo):
+def cadastraVeiculo(veiculo):
 	lista_veiculos.append(veiculo)
 	print(f"Veículo de placa: {veiculo.placa} cadastrado")
 
-def removerVeiculo(placa):
+def removeVeiculo(placa):
 	if len(lista_veiculos) != 0:
 		for x in lista_veiculos:
 			if x.placa == placa:
 				lista_veiculos.remove(x)
 	print(f'Veículo de placa: {placa} removido')
 
+def consultaVeiculo(placa):
+	if len(lista_veiculos) != 0:
+		for x in lista_veiculos:
+			if x.placa == placa:
+				print(f'Placa: {x.placa}, Marca: {x.marca}, Modelo: {x.modelo}, Ano: {x.ano}, Código: {x.cod}')
+
 print(primeiro_veiculo.imprimeVeiculo())
 print(primeiro_veiculo.imprimeFinalPlaca())
 
 print(segundo_veiculo.imprimeVeiculo())
 print(segundo_veiculo.imprimeFinalPlaca())
-cadastrarVeiculo(primeiro_veiculo)
-cadastrarVeiculo(segundo_veiculo)
+cadastraVeiculo(primeiro_veiculo)
+cadastraVeiculo(segundo_veiculo)
 print(len(lista_veiculos))
-removerVeiculo("ASB4232")
+removeVeiculo("TTB3141")
 print(len(lista_veiculos))
+consultaVeiculo("ASB4232")
