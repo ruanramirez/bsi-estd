3bd326d0a15630d62c4f78632513e7e20476702d
Ruan Ramírez <ruanramirez01@gmail.com>
2021-11-18 20:06:52 -0300

    Atividade 17 de novembro de 2021

--- 05-veiculo/veiculo.py
+++ 05-veiculo/veiculo.py
@@ -36,29 +36,30 @@ class Veiculo:
 	def imprimeFinalPlaca(self):
 		return self.placa[-1]
 
+
 primeiro_veiculo = Veiculo('ASB4232', 'Toyota', 'AB-1', 2018, 41455)
 segundo_veiculo = Veiculo('TTB3141', 'Honda', 'GX-1', 2017, 12145)
 
 lista_veiculos = []
 
-def inserir(veiculo):
-	if veiculo != None:
-		lista_veiculos.append(veiculo)
-
-def mostrarLista(lista):
-	for x in lista:
-		return x
+def cadastrarVeiculo(veiculo):
+	lista_veiculos.append(veiculo)
+	print(f"Veículo de placa: {veiculo.placa} cadastrado")
 
 def removerVeiculo(placa):
-	print(placa)
-
+	if len(lista_veiculos) != 0:
+		for x in lista_veiculos:
+			if x.placa == placa:
+				lista_veiculos.remove(x)
+	print(f'Veículo de placa: {placa} removido')
 
 print(primeiro_veiculo.imprimeVeiculo())
 print(primeiro_veiculo.imprimeFinalPlaca())
 
 print(segundo_veiculo.imprimeVeiculo())
 print(segundo_veiculo.imprimeFinalPlaca())
-
-inserir(primeiro_veiculo)
-print(mostrarLista(lista_veiculos))
-
+cadastrarVeiculo(primeiro_veiculo)
+cadastrarVeiculo(segundo_veiculo)
+print(len(lista_veiculos))
+removerVeiculo("ASB4232")
+print(len(lista_veiculos))
